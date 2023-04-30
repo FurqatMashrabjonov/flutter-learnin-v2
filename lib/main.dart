05331e659ad78d101d22984f6b4019b8e90c3050
FurqatMashrabjonov <php_lesson@mail.ru>
2023-04-30 15:33:36 +0500

    decoration of Container

---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import './transaction.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(
+          title: 'title',
+        ));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> transactions = [
+    Transaction('12', 'New Shoes', 19.99, DateTime.now()),
+    Transaction('23', 'Repair Laptop', 69.99, DateTime.now()),
+    Transaction('23', 'Repair Laptop', 69.99, DateTime.now()),
+    Transaction('23', 'Repair Laptop', 69.99, DateTime.now()),
+    Transaction('23', 'Repair Laptop', 69.99, DateTime.now()),
+    Transaction('23', 'Repair Laptop', 69.99, DateTime.now()),
+    Transaction('23', 'Repair Laptop', 69.99, DateTime.now()),
+    Transaction('23', 'Repair Laptop', 69.99, DateTime.now()),
+    Transaction('23', 'Repair Laptop', 69.99, DateTime.now()),
+    Transaction('23', 'Re pair Laptop', 69.99, DateTime.now()),
+    Transaction('23', 'Repair Laptop', 69.99, DateTime.now()),
+    Transaction('23', 'Repair Laptop', 69.99, DateTime.now()),
+    Transaction('23', 'Repair Laptop', 69.99, DateTime.now()),
+    Transaction('23', 'Repair Laptop', 69.99, DateTime.now()),
+    Transaction('23', 'Repair Laptop', 69.99, DateTime.now()),
+    Transaction('23', 'Repair Laptop', 69.99, DateTime.now()),
+    Transaction('23', 'Repair Laptop', 69.99, DateTime.now()),
+    Transaction('23', 'Repair Laptop', 69.99, DateTime.now()),
+    Transaction('23', 'Repair Laptop', 69.99, DateTime.now()),
+    Transaction('23', 'Repair Laptop', 69.99, DateTime.now()),
+    Transaction('23', 'Repair Laptop', 69.99, DateTime.now()),
+    Transaction('23', 'Repair Laptop', 69.99, DateTime.now()),
+    Transaction('23', 'Repair Laptop', 69.99, DateTime.now()),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Transactions App',
+            style: TextStyle(fontSize: 25),
+          ),
+        ),
+        body: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 40,
+              child: const Card(
+                color: Colors.blue,
+                child: Text('CHILD'),
+              ),
+            ),
+            ListView.builder(
+              itemCount: transactions.length,
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              itemBuilder: (BuildContext context, int index){
+                return Row(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15
+                      ),
+                      decoration: BoxDecoration(border: Border.all(color: Colors.blue, width: 2)),
+                      padding: const EdgeInsets.all(10),
+                      child: Text(transactions[index].amount.toString()),
+                    ),
+                    Column(
+                      children: [
+                        Text(transactions[index].title),
+                        Text(transactions[index].date.toString())
+                      ],
+                    )
+                  ],
+                );
+              },
+
+
+            )
+          ],
+        ));
+  }
+}
